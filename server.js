5f63e29cd0e44d01a50536b4fce98d4c48b460ad
Shobha-N <shobha.n89@wipro.com>
2017-06-05 18:21:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+// http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Hello, world!');
     
-}).listen(process.env.PORT || 8080);
+// }).listen(process.env.PORT || 8080);
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+    res.send('Hello World!');
+    res.end();
+}
